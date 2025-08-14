3e364161d58af691d4dc5019a2189b73343b80c0
Secondtonumb <kevingenghaopeng@gmail.com>
2025-08-14 18:06:11 +0900

    add run

--- run_large_gpu.sh
+++ run_large_gpu.sh
@@ -2,7 +2,7 @@
 #------ qsub option --------#
 #PBS -q regular-g
 #PBS -l select=1
-#PBS -l walltime=01:30:00
+#PBS -l walltime=01:00:00
 #PBS -W group_list=gm64
 #PBS -j oe
 source ~/.bashrc
@@ -11,9 +11,8 @@ cd /home/m64000/work/SSL_MDD
 conda activate sb
 
 python ver4_train.py \
-        hparams/train_l2_arctic_cano_perc_dual_enc.yaml \
+        hparams/new_train_cano_perc_dual_enc_hybrid_attention_ctc.yaml\
         --perceived_ssl_model wavlm_large \
-        --canonical_ssl_model wavlm_large \
+        --canonical_ssl_model hubert_large \
         --ENCODER_DIM 1024 \
-        --feature_fusion dual_ssl_enc \
-        --prefix ""
+        --feature_fusion dual_ssl_enc_hybrid_ctc_attention 
